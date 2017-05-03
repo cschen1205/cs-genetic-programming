0063dce4e573a77cb6710538814d57362cf4eb11
Caishun Chen <cschen1205@gmail.com>
2017-05-03 09:45:48 +0800

    Set up the vscode project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_genetic_programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
