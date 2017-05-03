e12c0f37d4ea3cc62df49d20bcd4ef3121e718f7
Caishun Chen <cschen1205@gmail.com>
2017-05-03 10:02:02 +0800

    Core of LGP implementation

---
+++ CSChen/LGP/AlgorithmModels/Crossover/LGPCrossoverInstruction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimuKit.Solvers.LGP.AlgorithmModels.Crossover
+{
+    using System.Xml;
+    using SimuKit.Solvers.LGP.ComponentModels;
+    public abstract class LGPCrossoverInstruction
+    {
+        public LGPCrossoverInstruction()
+        {
+
+        }
+
+        public LGPCrossoverInstruction(XmlElement xml_level1)
+        {
+
+        }
+
+        public abstract void Crossover(LGPPop pop, LGPProgram child1, LGPProgram child2);
+        public abstract LGPCrossoverInstruction Clone();
+    }
+}
---
+++ CSChen/LGP/AlgorithmModels/Mutation/LGPMutationInstructionFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimuKit.Solvers.LGP.AlgorithmModels.RegInit
+{
+    using System.Xml;
+    using SimuKit.Solvers.LGP.ComponentModels;
+    using SimuKit.Solvers.LGP.ProblemModels;
+
+    public abstract class LGPRegInitInstruction
+    {
+        public LGPRegInitInstruction()
+        {
+
+        }
+
+        public LGPRegInitInstruction(XmlElement xml_level1)
+        {
+
+        }
+
+        public abstract void InitializeRegisters(LGPRegisterSet reg_set, LGPConstantSet constant_set, LGPFitnessCase fitness_case);
+        public abstract LGPRegInitInstruction Clone();
+    }
+}
