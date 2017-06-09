77599ea291e83a25209eb19362320d738ab993ca
Caishun Chen <cschen1205@gmail.com>
2017-06-09 13:07:02 +0800

    get unit testing to work

--- lgp-unit-test/MexicanHatUnitTest.cs
+++ lgp-unit-test/MexicanHatUnitTest.cs
@@ -106,10 +106,42 @@ namespace lgp_unit_test
             return result;
         }
         
+        public static List<LGPFitnessCase> mexican_hat()
+        {
+            List<LGPFitnessCase> result = new List<LGPFitnessCase>();
+
+            Func<Double, Double, Double> mexican_hat_func = (x1, x2) => (1 - x1 * x1 / 4 - x2 * x2 / 4) * Math.Exp(-x1 * x2 / 8 - x2 * x2 / 8);
+
+            double lower_bound = -4;
+            double upper_bound = 4;
+            int period = 16;
+
+            double interval = (upper_bound - lower_bound) / period;
+
+            for (int i = 0; i < period; i++)
+            {
+                double x1 = lower_bound + interval * i;
+                for (int j = 0; j < period; j++)
+                {
+                    double x2 = lower_bound + interval * j;
+
+                    MexicanHatFitnessCase observation = new MexicanHatFitnessCase();
+
+                    observation.X1 = x1;
+                    observation.X2 = x2;
+                    observation.Y = mexican_hat_func(x1, x2);
+
+                    result.Add(observation);
+                }
+            }
+
+            return result;
+        }
+        
         [Fact]
         public void TestSymbolicRegression()
         {
-            List<LGPFitnessCase> table = new List<LGPFitnessCase>();
+            List<LGPFitnessCase> table = mexican_hat();
             LGPSchema config = new LGPSchema();
 
             LGPPop pop = new LGPPop(config);
@@ -119,6 +151,20 @@ namespace lgp_unit_test
             pop.OperatorSet.AddOperator(new LGPOperator_Multiplication());
             pop.OperatorSet.AddOperator(new LGPOperator_Power());
             pop.OperatorSet.AddIfltOperator();
+            
+            config.ConstantRegisters.Add(new KeyValuePair<double, double>(1, 1));
+            config.ConstantRegisters.Add(new KeyValuePair<double, double>(2, 1));
+            config.ConstantRegisters.Add(new KeyValuePair<double, double>(3, 1));
+            config.ConstantRegisters.Add(new KeyValuePair<double, double>(4, 1));
+            config.ConstantRegisters.Add(new KeyValuePair<double, double>(5, 1));
+            config.ConstantRegisters.Add(new KeyValuePair<double, double>(6, 1));
+            config.ConstantRegisters.Add(new KeyValuePair<double, double>(7, 1));
+            config.ConstantRegisters.Add(new KeyValuePair<double, double>(8, 1));
+            config.ConstantRegisters.Add(new KeyValuePair<double, double>(9, 1));
+
+            config.RegisterCount = 6;
+
+            config.MaxGenerations = 10;
 
             pop.CreateFitnessCase += (index) => table[index];
 
